e13b026060809a08af604d9c10ef6e9a6c5a8ba2
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2021-10-08 12:33:44 +0530

    Create q3.c

---
+++ Assignment/Assignment 1/q3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void convert_1D_to_2D(int arr[], int rows, int columns)
+{
+    int m = rows, n = columns;
+    int arr2[m][n];
+    int k = 0;
+    for (int i = 0; i < m; i++)
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+            arr2[i][j] = arr[k];
+            k++;
+        }
+    }
+    printf("Printing 2D array :\n");
+    for (int i = 0; i < rows; i++)
+    {
+
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d \t", arr2[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int arr[100], n;
+    printf("Enter the size of 1D array :\n");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int rows, columns;
+    printf("Enter the no of rows and columns\n");
+    scanf("%d %d", &rows, &columns);
+    printf("Printing Elements of 1D array\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+    convert_1D_to_2D(arr, rows, columns);
+
+    return 0;
+}

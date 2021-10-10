95fa9b8b7c9286181e784094a734d67dbfbd90d6
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2021-10-10 22:01:13 +0530

    Update q4.c

--- Assignment/Assignment 1/q4.c
+++ Assignment/Assignment 1/q4.c
@@ -1,67 +1,78 @@
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-void selection_sort(long long int a[],long long int n)
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+//Logic for selection sort
+
+void selectionsort(long long int a[], long long int n)
 {
-    for(int i=0;i<n-1;i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int j=i+1;j<n;j++)
+        int min = i;
+        for (int j = i + 1; j < n; j++)
         {
-            if(a[j]<a[i])
+            if (a[j] < a[min])
             {
-                int temp=a[j];
-                a[j]=a[i];
-                a[i]=temp;
+                min = j;
             }
+            int temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
         }
     }
-    printf("Array after using Selection Sort:\n");
-    for(int i=0;i<n;i++)
-        printf("%d\n ",a[i]);
+
+    //Printing sorted array
+    printf("Selection Sorted Array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
     return;
 }
-void bubble_sort(long long int a[],long long int n)
+
+//Logic for buuble sort
+
+void bubblesort(long long int a[], long long int n)
 {
-    int counter=1;
-    while(counter<n)
+
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int i=0;i<n-counter;i++)
+        for (int j = 0; j < n - i; j++)
         {
-            if(a[i]>a[i+1])
+            if (a[j] > a[j + 1])
             {
-                int temp=a[i];
-                a[i]=a[i+1];
-                a[i+1]=temp;
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
-        counter++;
     }
+    //printing sorted array
     printf("Bubble Sorted Array:\n");
-    for(int i=0;i<n;i++)
-        printf("%d ",a[i]);
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
     printf("\n");
     return;
 }
 int main()
 {
-    printf("Enter no of elements:\n");
+    printf("Enter the number of elements:\n");
     long long int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     long long int a[n];
-    for(int i=0;i<n;i++)
-        a[i]=i;
-    clock_t start,end;
+    for (int i = 0; i < n; i++)
+        a[i] = i;
+    clock_t start, end;
     long int t;
-    start=clock();
-    selection_sort(a,n);
-    end=clock();
-    t=end-start;
-    printf("Total time elapsed for selection sort: %f\n",(double)t/(double)CLOCKS_PER_SEC);
-    clock_t start2,end2;
+    start = clock();
+    selectionsort(a, n);
+    end = clock();
+    t = end - start;
+    printf("Total time elapsed for selection sort: %f\n", (double)t / (double)CLOCKS_PER_SEC);
+    clock_t start2, end2;
     long int t2;
-    start2=clock();
-    bubble_sort(a,n);
-    end2=clock();
-    t2=end2-start2;
-     printf("Total time elapsed for bubble sort: %f\n",(double)t2/(double)CLOCKS_PER_SEC);
+    start2 = clock();
+    bubblesort(a, n);
+    end2 = clock();
+    t2 = end2 - start2;
+    printf("Total time elapsed for bubble sort: %f\n", (double)t2 / (double)CLOCKS_PER_SEC);
 }

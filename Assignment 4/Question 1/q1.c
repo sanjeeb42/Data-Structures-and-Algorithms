533940052433ab721cb6654b75028db65bbd08f9
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2021-10-24 15:04:26 +0530

    Update q1.c

--- Assignment 4/Question 1/q1.c
+++ Assignment 4/Question 1/q1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#define MAXIMUM 200
 
+long long int Calculate_Hash(char input[MAXIMUM])
+{
+    long long int hash = 0;
+    for (int i = 0; i < MAXIMUM && input[i] != '\0'; i++)
+    {
+        hash += (i + 1) * input[i];
+    }
+    return hash;
+}
+
+int main()
+{
+    char str[MAXIMUM] = {0};
+    scanf("%[^\n]", str);
+    printf("%lld\n", Calculate_Hash(str));
+    return 0;
+}

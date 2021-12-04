0219d4b368e5e51981e7afe24aa602761aa13f96
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2021-12-04 11:55:19 +0530

    Update 1.Merge_Sort.c

--- Sorting_Algorithm/1.Merge_Sort.c
+++ Sorting_Algorithm/1.Merge_Sort.c
@@ -1,1 +1,78 @@
+#include <stdio.h>
 
+
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+void merge(int arr[], int start, int end)
+{
+    int mid=(start+end)/2;
+    int temp[100];
+    int i = start, j = mid + 1, k = start;
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] < arr[j])
+        {
+            temp[k++] = arr[i++];
+        }
+
+        else
+        {
+            temp[k++] = arr[j++];
+        }
+    }
+    while(i<=mid){
+        temp[k++]=arr[i++];
+    }
+    while(j<=end){
+        temp[k++]=arr[j++];
+    }
+    for(int i=start; i<=end; i++){
+        arr[i]=temp[i];
+    }
+}
+
+void mergesort(int arr[],int start,int end){
+    if(start>=end){
+        return ;
+    }
+
+    // Divide 
+    int mid=(start+end)/2;
+    mergesort(arr,start,mid);
+    mergesort(arr,mid+ 1, end);
+
+    // Conquer -- Merge
+    merge(arr,start,end);
+}
+
+
+int main()
+{
+    int n;
+    n=6;
+   
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    mergesort(arr,0,n-1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+    for(int i=n-1; i>=0; i--){
+        printf("%d\t",arr[i]);
+    }
+    printf("\n");
+    
+
+
+    return 0;
+}
